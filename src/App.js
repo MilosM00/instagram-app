019799cca32960472b8cbe188b4c4e6bd6af7f7b
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-02 10:49:48 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Display from "./components/Display";
 
 const App = () =>{
+
+    const [home, setHome] = React.useState(false);
+    const [explore, setExplore] = React.useState(false);
+    const [profile, setProfile] = React.useState(false);
+
+    const displayHome = () =>{
+        setHome(true);
+        setExplore(false);
+        setProfile(false);
+    };
+    
+    const displayExplore = () =>{
+        setHome(false);
+        setExplore(true);
+        setProfile(false);
+    };
+
+    const displayProfile = () =>{
+        setHome(false);
+        setExplore(false);
+        setProfile(true);
+    };
+
     return(
 
         <div>
 
-            <Navbar />
+            <Navbar 
+                displayHome={displayHome}
+                displayExplore={displayExplore}
+                displayProfile={displayProfile}
+            />
+
+            <Display 
+                home={home}
+                explore={explore}
+                profile={profile}
+            />
 
         </div>
 
