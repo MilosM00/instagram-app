3c949cb077dc54cc07290b81734e2032c3639c92
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-03 09:25:23 +0200

    users-js updated

---
+++ src/data/users.js
@@ -0,0 +1,38 @@
+import profileImageOne from "../images/user1.jpg";
+import profileImageTwo from "../images/user2.jpg";
+import profileImageThree from "../images/user3.jpg";
+
+export default [
+    {
+        username: "john_johnson",
+        password: "john123",
+        firstName: "John",
+        lastName: "Johnson",
+        profileImage: profileImageOne,
+        posts: 6,
+        followers: 641,
+        following: 611
+    },
+
+    {   
+        username: "jason_pink",
+        password: "jason123",
+        firstName: "Jason",
+        lastName: "Pink",
+        profileImage: profileImageTwo,
+        posts: 8,
+        followers: 842,
+        following: 263
+    },
+
+    {
+        username: "jessica_brown",
+        password: "jessica123",
+        firstName: "Jesicca",
+        lastName: "Brown",
+        profileImage: profileImageThree,
+        posts: 22,
+        followers: 1592,
+        following: 519
+    }
+];
