8d67015145b012f125863341f2eb4fd298256b64
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-05 11:11:25 +0200

    users-js updated

--- src/data/users.js
+++ src/data/users.js
@@ -9,6 +9,7 @@ export default [
         firstName: "John",
         lastName: "Johnson",
         profileImage: profileImageOne,
+        description: "Hello, my name is John it is pleasure to be here!",
         posts: 6,
         followers: 641,
         following: 611
@@ -20,6 +21,7 @@ export default [
         firstName: "Jason",
         lastName: "Pink",
         profileImage: profileImageTwo,
+        description: "Hey! My name is Jason, it feels nice to be here!",
         posts: 8,
         followers: 842,
         following: 263
@@ -31,6 +33,7 @@ export default [
         firstName: "Jesicca",
         lastName: "Brown",
         profileImage: profileImageThree,
+        description: "Hi, my name is Jessica, let's connect!",
         posts: 22,
         followers: 1592,
         following: 519
