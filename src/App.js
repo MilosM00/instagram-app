66359dd54bb848027462cb4b8cc11de98c3138be
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-05 11:09:34 +0200

    app-js updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Display from "./components/Display";
+import users from "./data/users";
 
 const App = () =>{
 
@@ -10,29 +11,62 @@ const App = () =>{
 
     const [login, setLogin] = React.useState(true);
 
-    const displayHomeLogo = () =>{
-        setHome(true);
-        setExplore(false);
-        setProfile(false);
-        document.body.scrollIntoView();
-    };
-
     const displayHome = () =>{
         setHome(true);
         setExplore(false);
         setProfile(false);
+        document.body.scrollIntoView();
     };
     
     const displayExplore = () =>{
         setHome(false);
         setExplore(true);
         setProfile(false);
+        document.body.scrollIntoView();
     };
 
     const displayProfile = () =>{
         setHome(false);
         setExplore(false);
         setProfile(true);
+        document.body.scrollIntoView();
+    };
+
+    const [username, setUsername] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const usernameEvent = (e) =>{
+        setUsername(e.target.value);
+    }
+
+    const passwordEvent = (e) =>{
+        setPassword(e.target.value);
+    }
+
+    let currentUser = users.find((value) => value.username === username)
+
+    const loginEvent = () =>{
+        if(currentUser?.password === password)
+        {
+            setLogin(false);
+        }
+    };
+
+    const logoutEvent = () =>{
+        document.body.scrollIntoView();
+        setHome(true);
+        setLogin(true);
+        setHamburger(false);
+        setExplore(false);
+        setProfile(false);
+        setUsername("");
+        setPassword("");
+    };
+
+    const [hamburger, setHamburger] = React.useState(false);
+
+    const hamburgerEvent = () =>{
+        setHamburger(!hamburger);
     };
 
     return(
@@ -43,7 +77,10 @@ const App = () =>{
                 displayHome={displayHome}
                 displayExplore={displayExplore}
                 displayProfile={displayProfile}
-                displayHomeLogo={displayHomeLogo}
+                currentUser={currentUser}
+                logoutEvent={logoutEvent}
+                hamburger={hamburger}
+                hamburgerEvent={hamburgerEvent}
             />
 
             <Display 
@@ -51,6 +88,13 @@ const App = () =>{
                 explore={explore}
                 profile={profile}
                 login={login}
+                setLogin={setLogin}
+                usernameEvent={usernameEvent}
+                passwordEvent={passwordEvent}
+                loginEvent={loginEvent}
+                password={password}
+                username={username}
+                currentUser={currentUser}
             />
 
         </div>
