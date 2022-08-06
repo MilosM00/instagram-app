d052f352b40183bb8a18e9b2bdf26799236804a9
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-06 10:08:42 +0200

    navbar-js updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,33 +5,36 @@ import imageLogo from "../images/instagram-logo.png";
 const Navbar = ({displayHome, displayExplore, displayProfile, currentUser, logoutEvent, hamburger, hamburgerEvent}) =>{
     return(
 
-        <div className="navbar">
-            
-            <img src={imageLogo} onClick={displayHome} alt="instagram" className="image-instagram-logo" />
+        <>
+            <div className="navbar">
+                
+                <img src={imageLogo} onClick={displayHome} alt="instagram" className="image-instagram-logo" />
 
-            <div className="input-search">
-                <i className="fa fa-search"></i>
-                <input type="text" placeholder="Search" />
+                <div className="input-search">
+                    <i className="fa fa-search"></i>
+                    <input type="text" placeholder="Search" />
+                </div>
+
+                <div className="inline-navbar">
+                    <p onClick={displayHome}><i className="fa fa-home"></i></p>
+                    <p onClick={displayExplore}><i className="fa fa-compass"></i></p>
+                    <img 
+                        src={currentUser?.profileImage} 
+                        alt="profile" 
+                        className="image-profile" 
+                        onClick={displayProfile} 
+                    />
+                    <p onClick={logoutEvent}><i className="fa fa-door-open"></i></p>
+                </div>
+
+                <p 
+                    className="hamburger" 
+                    onClick={hamburgerEvent}
+                >
+                    {hamburger === true ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
+                </p>
+
             </div>
-
-            <div className="inline-navbar">
-                <p onClick={displayHome}><i className="fa fa-home"></i></p>
-                <p onClick={displayExplore}><i className="fa fa-compass"></i></p>
-                <img 
-                    src={currentUser?.profileImage} 
-                    alt="profile" 
-                    className="image-profile" 
-                    onClick={displayProfile} 
-                />
-                <p onClick={logoutEvent}><i className="fa fa-door-open"></i></p>
-            </div>
-
-            <p 
-                className="hamburger" 
-                onClick={hamburgerEvent}
-            >
-                {hamburger === true ? <i className="fa fa-times"></i> : <i className="fa fa-bars"></i>}
-            </p>
 
             {hamburger === true &&
                 <div className="hamburger-list">
@@ -49,8 +52,8 @@ const Navbar = ({displayHome, displayExplore, displayProfile, currentUser, logou
                 </div>
             }
 
-        </div>
-
+        </>
+        
     );
 };
 
