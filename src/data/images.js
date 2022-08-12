460041b305e36e704eeb0c348aa10a1fb247c221
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-12 15:00:07 +0200

    images-js updated

---
+++ src/data/images.js
@@ -0,0 +1,27 @@
+import imageOne from "../images/instagram-image1.jpg";
+import imageTwo from "../images/instagram-image2.jpg";
+import imageThree from "../images/instagram-image3.jpg";
+import imageFour from "../images/instagram-image4.jpg";
+import imageFive from "../images/instagram-image5.jpg";
+
+export default [
+    {
+        image:imageOne
+    },
+
+    {
+        image:imageTwo
+    },
+
+    {
+        image:imageThree
+    },
+
+    {
+        image:imageFour
+    },
+
+    {
+        image:imageFive
+    }
+];
