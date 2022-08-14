bbafca4e4ef4292e1ec240884604f7241af85533
Milos Milijasevic <milosrm20005@gmail.com>
2022-08-14 14:39:14 +0200

    users-js updated

--- src/data/users.js
+++ src/data/users.js
@@ -59,181 +59,91 @@ export default [
 
     {
         username: "james_blue",
-        firstName: "James",
-        lastName: "Blue",
         profileImage: profileImageFour,
-        story: true,
-        description: "Hi, my name is James, let's connect!",
-        posts: 5,
-        followers: 1092,
-        following: 819
+        story: true
     },
 
     {
         username: "ana_white",
-        firstName: "Ana",
-        lastName: "White",
         profileImage: profileImageFive,
-        story: true,
-        description: "Hello, my name is Ana, follow me!",
-        posts: 15,
-        followers: 1292,
-        following: 419
+        story: true
     },
 
     {
         username: "dennise_johnson",
-        firstName: "Dennise",
-        lastName: "Johnson",
         profileImage: profileImageSix,
-        story: true,
-        description: "Hey! My name is Dennise, love to be here!",
-        posts: 25,
-        followers: 2092,
-        following: 754
+        story: true
     },
 
     {
         username: "michael_smith",
-        firstName: "Michael",
-        lastName: "Smith",
         profileImage: profileImageSeven,
-        story: true,
-        description: "Hello, my name is Michael, it is pleasure to be here!",
-        posts: 5,
-        followers: 1092,
-        following: 819
+        story: true
     },
     
     {
         username: "will_jones",
-        firstName: "Will",
-        lastName: "Jones",
         profileImage: profileImageEight,
-        story: true,
-        description: "Hello, my name is Will, let's connect!",
-        posts: 3,
-        followers: 492,
-        following: 419
+        story: true
     },
 
     {
         username: "nick_davis",
-        firstName: "Nick",
-        lastName: "Davis",
         profileImage: profileImageNine,
-        story: true,
-        description: "Hi, my name is Nick, it is good to be here!",
-        posts: 6,
-        followers: 892,
-        following: 819
+        story: true
     },
 
     {
         username: "rechelle_miller",
-        firstName: "Rechelle",
-        lastName: "Miller",
         profileImage: profileImageTen,
-        story: true,
-        description: "Hi, my name is Rechelle, follow me!",
-        posts: 17,
-        followers: 1592,
-        following: 769
+        story: true
     },
 
     {
         username: "pam_clark",
-        firstName: "Pam",
-        lastName: "Clark",
         profileImage: profileImageEleven,
-        story: true,
-        description: "Hello, my name is Pam, let's connect!",
-        posts: 10,
-        followers: 1792,
-        following: 989
+        story: true
     },
 
     {
         username: "sara_lee",
-        firstName: "Sara",
-        lastName: "Lee",
         profileImage: profileImageTwelve,
-        story: true,
-        description: "Hey! My name is Sara, follow me!",
-        posts: 30,
-        followers: 3592,
-        following: 1152
+        story: true
     },
 
     {
         username: "roy_martin",
-        firstName: "Roy",
-        lastName: "Martin",
         profileImage: profileImageThirteen,
-        story: true,
-        description: "Hello! My name is Roy, it feels nice to be here!",
-        posts: 5,
-        followers: 921,
-        following: 679
+        story: true
     },
 
     {
         username: "sophia_taylor",
-        firstName: "Sophia",
-        lastName: "Taylor",
         profileImage: profileImageFourteen,
-        story: true,
-        description: "Hey! My name is Sophia, follow me!",
-        posts: 30,
-        followers: 4392,
-        following: 1249
+        story: true
     },
 
     {
         username: "emma_lewis",
-        firstName: "Emma",
-        lastName: "Lewis",
         profileImage: profileImageFifteen,
-        story: true,
-        description: "Hello, my name is Emma, let's connect!",
-        posts: 16,
-        followers: 2522,
-        following: 1253
+        story: true
     },
     
     {
         username: "jim_adams",
-        firstName: "Jim",
-        lastName: "Adams",
         profileImage: profileImageSixteen,
-        story: true,
-        description: "Hey! My name is Jim, let's connect!",
-        posts: 7,
-        followers: 992,
-        following: 819
+        story: true
     },
 
     {
         username: "mia_morris",
-        firstName: "Mia",
-        lastName: "Morris",
         profileImage: profileImageSeventeen,
-        story: true,
-        description: "Hello! My name is Mia, love to be here!",
-        posts: 21,
-        followers: 3192,
-        following: 1821
+        story: true
     },
 
     {
         username: "ellis_hall",
-        firstName: "Ellis",
-        lastName: "Hall",
         profileImage: profileImageEighteen,
-        story: true,
-        description: "Hi, my name is Ellis, let's connect!",
-        posts: 5,
-        followers: 1292,
-        following: 729
+        story: true
     }
 ];
